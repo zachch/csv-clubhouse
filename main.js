034daa08405c985d06109b3c4635e7a02e890b35
zachch <zachch@users.noreply.github.com>
2017-04-30 00:01:54 +0800

    Pull everything together in the main.js file

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 (async function(){
   "use strict"
+  var stories = await clubhouseHelper.getStories()
+  stories = clubhouseHelper.processStories(stories)
+  var csvContent = utils.getCSVContent(stories,clubhouseHelper.getHeaders())
+  utils.downloadCSVFile(csvContent, 'clubhouse.csv')
 
 })()
